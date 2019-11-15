f13a6e857798c7761acc339f112a4f1fb2bc7ea2
lutfidmz <itsmafuturegate@gmail.co>
2019-11-15 16:56:23 +0700

    add routing and firebase context

---
+++ src/config/Firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfW8KCDx6nJpIXmLHM3mba6gx8lpgsMig",
+    authDomain: "aplikasi-penjualan-4be51.firebaseapp.com",
+    databaseURL: "https://aplikasi-penjualan-4be51.firebaseio.com",
+    projectId: "aplikasi-penjualan-4be51",
+    storageBucket: "aplikasi-penjualan-4be51.appspot.com",
+    messagingSenderId: "201056266238",
+    appId: "1:201056266238:web:9573de3de450f9047742af",
+    measurementId: "G-7ELN3NY6Z4"
+  };
+
+  export default firebaseConfig
